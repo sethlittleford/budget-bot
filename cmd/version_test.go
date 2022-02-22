bebd53c1326dd6869ddeb68444ced486c2459a65
Seth Littleford <seth.littleford@getweave.com>
2022-02-22 09:56:00 -0700

    must set args to command for test

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 )
 
+// TestVersionCmd must be run with go test -ldflags (see Makefile for details)
 func TestVersionCmd(t *testing.T) {
+	rootCmd.SetArgs([]string{"version"})
 	if err := versionCmd.Execute(); err != nil {
 		t.Fatalf("Version Command failed with error = %v", err)
 	}
