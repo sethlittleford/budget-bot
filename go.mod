5d1720c70fded6680e4c1c3b8d689f9567c980cf
Seth Littleford <seth.littleford@getweave.com>
2022-02-25 15:52:17 -0700

    environment variables for plaid secrets

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sethlittleford/budget-bot
 
 go 1.16
 
-require github.com/spf13/cobra v1.3.0
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/plaid/plaid-go/v2 v2.0.1
+	github.com/spf13/cobra v1.3.0
+)
--- main.go
+++ main.go
@@ -4,8 +4,18 @@ Copyright © 2022 Seth Littleford <seth.littleford@gmail.com>
 */
 package main
 
-import "github.com/sethlittleford/budget-bot/cmd"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/sethlittleford/budget-bot/cmd"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file from root")
+	}
+
 	cmd.Execute()
 }
